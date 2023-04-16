8119e55b8dcd3f638b6d4727093efe77d1ae1c4b
MaesTro <kontchou05@gmail.com>
2023-04-16 16:48:51 +0200

    initialization

---
+++ src/main/java/com/backend/chatop/TestController.java
@@ -0,0 +1,13 @@
+package com.backend.chatop;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @RequestMapping("/")
+    public String test(){
+        return "test";
+    }
+}
