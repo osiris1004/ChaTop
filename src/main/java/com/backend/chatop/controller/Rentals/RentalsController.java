3ddbe642741e84ac649c731ea229cb804ad88682
MaesTro <kontchou05@gmail.com>
2023-05-12 17:15:18 +0200

    slight update

--- src/main/java/com/backend/chatop/controller/Rentals/RentalsController.java
+++ src/main/java/com/backend/chatop/controller/Rentals/RentalsController.java
@@ -2,14 +2,10 @@
 package com.backend.chatop.controller.Rentals;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
